4f31a0b0ad61975016ecf64fdc32ff3039048da9
cw7430 <cw282@naver.com>
2025-05-07 17:59:50 +0900

    2025-05-07

--- src/constants/response/index.ts
+++ src/constants/response/index.ts
@@ -1,4 +1,3 @@
-import ResponseMessage from "./response_message";
 import ResponseHttpStatus from "./response_http_status";
 
-export { ResponseMessage, ResponseHttpStatus };
+export { ResponseHttpStatus };
