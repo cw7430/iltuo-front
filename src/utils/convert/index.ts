c8e7360d137c07dc8a90c7513490b162a7354513
cw7430 <cw282@naver.com>
2025-06-09 20:03:44 +0900

    Update index.ts

--- src/utils/convert/index.ts
+++ src/utils/convert/index.ts
@@ -1,5 +1,5 @@
 import convertUtcToLocalDate from "./date/convert_utc_to_local_date";
-import convertAuthProvider from "./string/convert_auth_provider";
+import convertAuthProvider from "./string/convert_auth_Provider";
 import convertOrderStatus from "./string/convert_order_status";
 
 export { convertUtcToLocalDate, convertAuthProvider, convertOrderStatus };
